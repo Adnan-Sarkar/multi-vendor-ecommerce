75dac2e357888060f6fa53402eea4448e2ff97ec
Adnan-Sarkar <adnansarkar111@gmail.com>
2026-06-10 02:57:12 +0600

    feat: create common api response trait

---
+++ backend/app/Traits/ApiResponse.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Traits;
+
+class ApiResponse
+{
+    public function success($data, $message = 'Success', $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+        ], $code);
+    }
+
+    public function error($message = 'Error', $code = 400)
+    {
+        return response()->json([
+            'success' => false,
+            'message' => $message,
+            'data' => null,
+        ], $code);
+    }
+}
