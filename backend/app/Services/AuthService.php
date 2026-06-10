a81eac2fde453ed96f904d1a0713e5a498e4b006
Adnan-Sarkar <adnansarkar111@gmail.com>
2026-06-10 12:56:30 +0600

    fix: update register service using user resource

--- backend/app/Services/AuthService.php
+++ backend/app/Services/AuthService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Http\Resources\Api\V1\UserResource;
 use App\Repositories\AuthRepository;
 
 class AuthService
@@ -18,13 +19,14 @@ class AuthService
 
     public function register(array $data) {
         $data['role'] = 'customer';
+        $data['is_active'] = true;
         $user = $this->authRepository->createUser($data);
 
         $user->assignRole('customer');
         $token = $user->createToken('auth_token')->plainTextToken;
 
         return [
-            'user' => $user,
+            'user' => new UserResource($user),
             'token' => $token,
         ];
     }
