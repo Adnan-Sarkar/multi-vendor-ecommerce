12c9c71a0648b8ceefb3198c9a5c63ecc9309104
Adnan-Sarkar <adnansarkar111@gmail.com>
2026-06-11 14:43:06 +0600

    fix: update api response trait with default error code

--- backend/app/Traits/ApiResponse.php
+++ backend/app/Traits/ApiResponse.php
@@ -2,9 +2,11 @@
 
 namespace App\Traits;
 
+use Illuminate\Http\JsonResponse;
+
 trait ApiResponse
 {
-    public function success($data, $message = 'Success', $code = 200)
+    public function success($data, $message = 'Success', $code = 200): JsonResponse
     {
         return response()->json([
             'success' => true,
@@ -13,8 +15,10 @@ trait ApiResponse
         ], $code);
     }
 
-    public function error($message = 'Error', $code = 400)
+    public function error($message = 'Error', $code = 400): JsonResponse
     {
+        $code = ($code && $code >= 100 && $code <= 599) ? $code : 400;
+
         return response()->json([
             'success' => false,
             'message' => $message,
